329ff9138fb96edbeccd436a7e2bc820cae5182f
Aditya Upadhyay <qcxwae@gmail.com>
2026-03-24 10:26:26 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
+import { redirect } from 'next/navigation'
+
 export default function Home() {
-  return (
-    <iframe 
-      src="/portfolio.html"
-      width="100%"
-      height="100vh"
-      style={{ border: "none" }}
-    />
-  )
+  redirect('/portfolio.html')
 }
